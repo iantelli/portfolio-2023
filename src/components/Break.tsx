bce7ed772fc1d4bf15d0967c6eae1a9af328fa3f
iantelli <2ndex@duck.com>
2023-03-25 13:27:33 -0700

    small fix

--- src/components/Break.tsx
+++ src/components/Break.tsx
@@ -10,7 +10,7 @@ export default function Break({ aniDelay }: BreakProps) {
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
-            transition={{ delay: aniDelay, duration: 1 }}
+            transition={{ delay: aniDelay, duration: 0.5 }}
         >
             <hr className={"border-rosePineDawn-subtle dark:border-rosePine-subtle w-5/6 border-1 my-4 lg:hidden"} />
         </motion.div>
