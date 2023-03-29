0ccc8c7a0851d6f580fdbdde238cf9a79df3f56e
iantelli <2ndex@duck.com>
2023-03-28 20:31:40 -0700

    small fix

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
-        transition={{ delay: 0, duration: 1 }}
+        transition={{ delay: 0, duration: 0.5 }}
       >
         Project Showcase
       </motion.h1>
